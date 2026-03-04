036551c8e6ba26f5ae7a397500cba71038936fdc
acl13 <128934431+acl13@users.noreply.github.com>
2026-03-03 19:20:26 -0500

    add StyleSheet

--- clt-bookstore-crawl/app/index.tsx
+++ clt-bookstore-crawl/app/index.tsx
@@ -1,9 +1,15 @@
 "use client";
-import { Text, View, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { fetchBookstores } from "./store/slices/bookstoreData";
 import { useAppDispatch } from "./store/configureStore";
 import { useSelector } from "react-redux";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 type Bookstore = {
   _id: string;
@@ -23,33 +29,67 @@ export default function Index(this: any) {
         data: Bookstore[];
         isLoading: boolean;
       };
-    }) => state.bookstores.data
+    }) => state.bookstores.data,
   );
 
   useEffect(() => {
     dispatch(fetchBookstores());
   }, [dispatch]);
 
-  const logStores = () => {
-    console.log(bookstores);
+  const [selectedItems, setSelectedItems] = useState<Bookstore[]>([]);
+
+  const handleSelect = (item: Bookstore) => {
+    // Prevent duplicates
+    if (!selectedItems.find((selected) => selected._id === item._id)) {
+      setSelectedItems([...selectedItems, item]);
+    }
   };
 
   const renderItem = ({ item }: { item: Bookstore }) => (
-    <View>
-      <Text>{item.name}</Text>
-    </View>
+    <TouchableOpacity onPress={() => handleSelect(item)}>
+      <View
+        style={{
+          padding: 10,
+          backgroundColor: "#f0f0f0",
+          marginVertical: 5,
+          borderRadius: 5,
+        }}
+      >
+        <Text>{item.name}</Text>
+      </View>
+    </TouchableOpacity>
   );
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <FlatList data={bookstores} renderItem={renderItem} />
-      <button onClick={logStores}>Log stores</button>
+    <View style={styles.container}>
+      <FlatList
+        data={bookstores}
+        renderItem={renderItem}
+        keyExtractor={(item) => item._id}
+        numColumns={3}
+      />
+      <Text style={{ marginTop: 20, fontWeight: "bold" }}>
+        Selected Bookstores:
+      </Text>
+      {selectedItems.map((store) => (
+        <Text key={store._id}>{store.name}</Text>
+      ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    alignItems: "center",
+  },
+  card: {
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "600",
+  },
+});
